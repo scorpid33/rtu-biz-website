9cd10e09498be936f48f82c901e2cc40dba43c5c
scorpid33 <scorpid33+1@gmail.com>
2019-12-10 13:26:59 +0200

    Change WP root on commit

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 set -e
 
+docker-compose exec rtu_biz sed -i 's/3.125.8.106/localhost:8003/' wp-config.php
 docker commit -m "$1" rtu-biz-website_rtu_biz_1 scorpid33/rtu-biz-website
 docker commit -m "$1" rtu-biz-website_db_1 scorpid33/rtu-biz-website-db
+docker-compose exec rtu_biz sed -i 's/localhost:8003/3.125.8.106/' wp-config.php
 
 docker push scorpid33/rtu-biz-website
 docker push scorpid33/rtu-biz-website-db
